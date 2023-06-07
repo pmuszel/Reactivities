b205c19d002955816664cd874d071073e0b7dbdc
Piotr Muszel <piotr.muszel@supremis.pl>
2023-06-07 15:03:57 +0200

    End of section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { Button, Header, List, ListItem } from 'semantic-ui-react';
+
+function App() {
+
+const [activities, setActivities] = useState([]);
+
+useEffect(() => {
+  axios.get('http://localhost:5000/api/activities')
+    .then(response => {
+      setActivities(response.data);
+    })
+}, []);
+
+  return (
+    <>
+      <Header as='h2' icon='users' content='Reactivities' />
+        <List>
+          {activities.map((activity: any) => (
+            <ListItem key={activity.id}>
+              {activity.title}
+            </ListItem>
+          ))}
+        </List>
+    </>
+  );
+}
+
+export default App;
